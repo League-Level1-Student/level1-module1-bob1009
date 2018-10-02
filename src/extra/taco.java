ef1f18859d4d5333feb77bbd424e24ac22a45d7c
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-01 17:49:17 -0700

    finished naming methods and taco but still need to work on person ;)

---
+++ src/extra/taco.java
@@ -0,0 +1,29 @@
+package extra;
+
+public class taco {
+	private String meat = null;
+	private String sauce = null;
+public static void main(String[] args) {
+	
+	//getsauce(sauce);
+	//getmeat(meat);
+}
+
+public String getmeat() {
+	// TODO Auto-generated method stub
+	return meat;
+	
+}
+
+public String getsauce() {
+	// TODO Auto-generated method stub
+	return sauce;
+}
+
+public void setSauce(String sauce) {
+	this.sauce=sauce;
+}
+public void setMeat(String meat) {
+	this.sauce=sauce;
+}
+}
