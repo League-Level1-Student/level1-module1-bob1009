1049e56d38e844e670d4a0a324a8755fee701f50
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-01 17:50:06 -0700

    getters an setters

--- src/extra/Person.java
+++ src/extra/Person.java
@@ -1,5 +1,15 @@
 package extra;
 
 public class Person {
-
+public static void main(String[] args) {
+	
 }
+String name;
+String superpower;
+
+public void setname(String name) {
+	this.name=name;
+	}
+public void setsuperpower(String superpower) {
+	this.superpower=superpower;
+}}
